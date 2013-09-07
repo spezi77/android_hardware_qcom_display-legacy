64f7e283caf2de0399ff9a60286cf39288b53a85
Colin Cross <ccross@android.com>
2013-09-06 18:03:02 -0700

    Initial msm8x64 memory tracker HAL
    Reads memory info from kgsl to track unaccounted for GL and ION
    memory.  Right now it has to parse every allocation and then
    cross-reference it against /proc/pid/smaps.  It would be faster
    to modify the kgsl driver to keep counters that could just be
    read directly.
    
    Bug: 10294768
    Change-Id: Icb17558f03ce53bc594fbee097e51fe82e15dc31
    
    Conflicts:
    
    	Android.mk

--- Android.mk
+++ Android.mk
@@ -1,10 +1,10 @@
 ifeq ($(call my-dir),$(call project-path-for,qcom-display))
 display-hals := libgralloc libgenlock libcopybit
 display-hals += libhwcomposer liboverlay libqdutils
+display-hals += libmemtrack
 ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
 display-hals += liblight
 endif
-
 include $(call all-named-subdir-makefiles,$(display-hals))
 
 endif
